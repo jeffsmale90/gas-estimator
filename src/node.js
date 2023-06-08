5d1d830d0bc965447be1f8e208cd662906adf76f
Jeff Smale <6363749+jeffsmale90@users.noreply.github.com>
2023-06-09 11:27:58 +1200

    Support for call stipend

--- src/node.js
+++ src/node.js
@@ -2,7 +2,7 @@ const SIXTY_FOURTHS = 63 / 64;
 
 function nodeFromData(data) {
   const children = data.children?.map((child) => nodeFromData(child));
-  const node = new Node(data.type, data.cost, data.req, children);
+  const node = new Node(data.type, data.cost, data.req, data.stipend, children);
 
   return node;
 }
@@ -11,12 +11,14 @@ class Node {
   type;
   cost;
   children;
-  req;
+  minimum;
+  stipend;
 
-  constructor(type, cost, req, children) {
+  constructor(type, cost, minimum, stipend, children) {
     this.type = type;
     this.cost = cost;
-    this.req = req || cost;
+    this.stipend = stipend;
+    this.minimum = minimum || cost;
     if (children) {
       this.children = children;
     }
@@ -26,32 +28,67 @@ class Node {
     if (this.children === undefined) {
       const result = {
         cost: this.cost,
-        req: this.req,
+        minimum: this.minimum,
       };
       console.log(`Fetching gas for ${this.type}`, result);
       return result;
     } else {
-      let totalReq = 0;
-      let totalCost = 0;
+      // the minimum and total cost of the subframe
+      let childMinimum = 0;
+      let childCost = 0;
       for (const child of this.children) {
-        const { req, cost } = child.requiredGas(true);
-        totalReq = Math.max(totalCost + req, totalReq);
+        const { minimum, cost } = child.requiredGas(true);
+        childMinimum = Math.max(childCost + minimum, childMinimum);
         // we need to carry the _actual_ cost forward, as that is what we spend
-        totalCost += cost;
+        childCost += cost;
       }
 
-      let req = totalReq;
+      // todo: SSTORE op cost may be greater than 2300 so a Math.max must be added somewhere
+
+      if (this.stipend) {
+        // Within the call frame, the gas limit will be the gas available + stipend.
+        // The minimum gas required at the start of the call frame is the minimum gas required for the children - stipend.
+        // The minimum gas required at the CALL opcode, will be the maximum of the CALL opcode gas, and
+        // 64/63 of the minimum gas required within the frame.
+
+        console.log(this.type, {
+          cost: this.cost,
+          minimum: this.minimum,
+          stipend: this.stipend,
+        });
+        console.log("Minimum required to execute child frame: " + childMinimum);
+        console.log(
+          "Minimum required at the start of the frame: " +
+            (childMinimum - this.stipend)
+        );
+        console.log(
+          "Minimum required at the end of the CALL* opcode: " +
+            Math.floor((childMinimum - this.stipend) / SIXTY_FOURTHS)
+        );
+
+        console.log(
+          "Minimum required for CALL* opcode: " +
+            (Math.floor((childMinimum - this.stipend) / SIXTY_FOURTHS) +
+              this.minimum)
+        );
+        childMinimum -= this.stipend;
+        childCost -= this.stipend;
+
+        // even though the minimum required to execute the child is < 0, we need
+        // enough to cover the CALL opcode. Total cost _can_ go negative though,
+        // because we get the stipend for free.
+        childMinimum = Math.max(0, childMinimum);
+        console.log({ childMinimum, childCost });
+      }
+
       if (applySixtyFloorths) {
-        req = Math.floor(totalReq / SIXTY_FOURTHS);
+        childMinimum = Math.floor(childMinimum / SIXTY_FOURTHS);
+        console.log({ childMinimum, childCost });
       }
 
-      console.log(`Calculating gas for ${this.type}`, {
-        cost: totalCost,
-        req,
-      });
       return {
-        cost: totalCost + this.cost,
-        req: req + this.req,
+        cost: childCost + this.cost,
+        minimum: childMinimum + this.minimum,
       };
     }
   }
